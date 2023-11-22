746762879fd1273fa3669092bc1461a2486446a7
burzza <bineshburzaa@gmail.com>
2023-11-22 10:34:54 +0545

    adding bird

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,39 +1,61 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double birdY = 0;
+
+  void jump() {
+    Timer.periodic(const Duration(milliseconds: 50), (timer) {
+      setState(() {
+        birdY -= 0.05;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              color: Colors.blue,
-              child: Center(
-                child: Stack(
-                  children: [
-                    Container(
-                      alignment: const Alignment(0, 1),
-                      child: Container(
-                        width: 50.0,
-                        height: 50.0,
-                        color: Colors.white,
-                      ),
-                    )
-                  ],
+    return GestureDetector(
+      onTap: jump,
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.blue,
+                child: Center(
+                  child: Stack(
+                    children: [
+                      Container(
+                        alignment: Alignment(0, birdY),
+                        child: const SizedBox(
+                          width: 100.0,
+                          height: 100.0,
+                          child: Image(
+                            image: AssetImage('assets/images/bird.png'),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.brown,
-            ),
-          )
-        ],
+            Expanded(
+              child: Container(
+                color: Colors.brown,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
